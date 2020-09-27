f1358e025933aa45690c103cd506712dbe9e5e83
Will Foran <willforan@gmail.com>
2020-09-27 13:13:27 -0400

    🚚 reorganize as module using rtrhd/ICA-AROMA as a template

---
+++ ica_aroma/__init__.py
@@ -0,0 +1,20 @@
+import os
+import ica_aroma
+
+__version__ = "0.3-beta"
+
+
+def get_data_path():
+    """find directory location of
+     mask_csf.nii.gz, mask_edge.nii.gz, and mask_out.nii.gz.
+
+    will allow env var AROMADIR to overwrite package location
+    @return path to data/ (string)
+    """
+    datadir = os.environ.get("AROMADIR", None) or \
+        os.path.join(ica_aroma.__loader__.path, "data")
+
+    assert os.path.isdir(datadir)
+    assert os.path.isfile(os.path.join(datadir, 'mask_csf.nii.gz'))
+
+    return datadir
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os.path import join, dirname
+
+def readme(fname):
+    with open(join(dirname(__file__), fname)) as f:
+        text = f.read()
+    return text
+
+
+setup(
+    name='icaaroma',
+    description='ICA-based Automatic Removal Of Motion Artefacts',
+    long_description=readme('README.md'),
+    author="Maarten Mennes",
+    author_email="m.mennes@donders.ru.nl",
+    version='0.4.0',
+    license='Apache Software License',
+    url="http://github.com/rtrhd/icaaroma",
+    keywords="ics fmri motion",
+    packages=['ica_aroma'],
+    install_requires=['nibabel>=1.3.0', 'matplotlib>=2.2', 'numpy>=1.14',
+                      'pandas>=0.23', 'seaborn>=0.9.0'],
+    tests_require=['nose'],
+    entry_points={'console_scripts': ['ica_aroma = ica_aroma.ica_aroma:main']},
+    package_data={'icaaroma': ['data/mask_*.nii.gz']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+    ]
+)
